1661a5813cd8787f9a4d0130d83c8c16272f5ab5
MrLexotan <leite.tavares.joao@gmail.com>
2021-09-14 14:41:56 -0300

    Adicionado script para parar a contagem

--- js.js
+++ js.js
@@ -1,12 +1,10 @@
 let min = 25
 let sec = 0
-
-
+const counter = document.querySelector('#timer')
+const start = document.querySelector('.btn:first-child')
+const stop = document.querySelector('.btn:nth-child(2)')
 
 function timer() {
-
-    const counter = document.querySelector('#timer')
-
     if (sec.toString() <= 0 ) {
         min--
         sec = 60
@@ -15,13 +13,35 @@ function timer() {
         sec--
     }
     if (min === 0) {
+        window.alert('De uma pausa')
         location.reload()
     }
     counter.innerHTML = (min.toString().length < 2 ? '0' + min : min) + ':' +
         (sec.toString().length < 2 ? '0' + sec : sec)
-
-    setInterval(timer, 100)
 }
 
+function inicializador () {
+    const interval = setInterval(timer, 100)
+    // clearInterval(interval)
+    if (stop.addEventListener('click', ()=> {clearInterval(interval)})){
+
+    }
+
+}
+
+function reset() {
+    location.reload()
+}
+
+start.addEventListener('click', inicializador )
+
+
+
+
+
+
+//
+// stop1.addEventListener('click', stop)
+// const reset1 = stop.nextElementSibling
 
 
